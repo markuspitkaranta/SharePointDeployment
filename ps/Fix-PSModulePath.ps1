b2092a720d95f11bbe8721630c40195960ba2951
mark.pitkaranta <mark.pitkaranta@avanade.com>
2017-08-31 20:07:35 +0300

    asd

--- ps/Fix-PSModulePath.ps1
+++ ps/Fix-PSModulePath.ps1
@@ -1,6 +1,7 @@
 ﻿$oldPath = (Get-ItemProperty "HKLM:\System\CurrentControlSet\Control\Session Manager\Environment" -Name PSModulePath).PSModulePath
 
 $newPath = $oldPath.Replace(";C:\Program Files\WindowsPowerShell\Modules;",";")
+$newPath = $newPath.Replace(";C:\Program Files\WindowsPowerShell\Modules\;",";")
 
 "test" >> c:\packages\test.log
 
