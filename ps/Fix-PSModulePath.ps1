724163d2cbfb7c554174ff80c08324cb60eb010a
mark.pitkaranta <mark.pitkaranta@avanade.com>
2017-08-31 19:30:21 +0300

    Script ext

---
+++ ps/Fix-PSModulePath.ps1
@@ -0,0 +1,7 @@
+﻿$oldPath = (Get-ItemProperty "HKLM:\System\CurrentControlSet\Control\Session Manager\Environment" -Name PSModulePath).PSModulePath
+
+$newPath = $oldPath.Replace(";C:\Program Files\WindowsPowerShell\Modules;",";")
+
+"test" >> c:\packages\test.log
+
+Set-ItemProperty "HKLM:\System\CurrentControlSet\Control\Session Manager\Environment" -Name PSModulePath -Value $newPath
